58069420a412d4a895bfdc32d92c5c68eaab8b1b
Dona-j <udyjimmy6@gmail.com>
2022-12-07 15:26:46 +0000

    Add task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,12 +3,12 @@
 if __name == "__main__":
 import calculator_1
 
-	"""print the result of some maths"""
+	"""print the result of addition, subtraction, multiplication and division of integers 10 & 5"""
 	
 	a = 10
 	b = 5
 	print("{} + {} = {}".format(a, b, add(a, b)))
-	print("{} - {} = {}".format(a, b, subtract(a, b)))
-	print("{} * {} = {}".format(a, b, multiply(a, b)))
-	print("{} / {} = {}".format(a, b, divide(a, b)))
+	print("{} - {} = {}".format(a, b, sub(a, b)))
+	print("{} * {} = {}".format(a, b, mul(a, b)))
+	print("{} / {} = {}".format(a, b, div(a, b)))
 
