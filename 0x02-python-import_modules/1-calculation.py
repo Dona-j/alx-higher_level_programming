5407ab1a5e896cf6e8fae5eb92fbb93c3404405f
Dona-j <udyjimmy6@gmail.com>
2022-12-07 15:31:37 +0000

    Add task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 if __name == "__main__":
-import calculator_1
-
 	"""print the result of addition, subtraction, multiplication and division of integers 10 & 5"""
-	
-	a = 10
+	from calculator_1 import add, sub, mul $ div
+
+        a = 10
 	b = 5
 	print("{} + {} = {}".format(a, b, add(a, b)))
 	print("{} - {} = {}".format(a, b, sub(a, b)))
