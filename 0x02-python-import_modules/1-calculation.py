aa6e0e6d0d0ae9788fcffb9819f60a9b3bf4eb9a
Dona-j <udyjimmy6@gmail.com>
2022-12-07 15:22:31 +0000

    Add task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name == "__main__":
+import calculator_1
+
+	"""print the result of some maths"""
+	
+	a = 10
+	b = 5
+	print("{} + {} = {}".format(a, b, add(a, b)))
+	print("{} - {} = {}".format(a, b, subtract(a, b)))
+	print("{} * {} = {}".format(a, b, multiply(a, b)))
+	print("{} / {} = {}".format(a, b, divide(a, b)))
+
