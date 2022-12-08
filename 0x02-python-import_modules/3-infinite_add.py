4df44b329566968b683cd72e6cc43118a43da20f
Dona-j <udyjimmy6@gmail.com>
2022-12-08 00:02:24 +0000

    Add  task files

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
+    """Print the sum of all arguments"""
     import sys
 
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    argv = sys.argv
+    res = 0
+    for i, arg in enumerate(argv):
+        if i == 0:
+            continue
+        res += int(arg)
+
+    print("{:d}".format(res))
