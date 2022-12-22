5134e126d5d5724f5dd1e00aa3e707842fbae70c
Dona-j <udyjimmy6@gmail.com>
2022-12-22 04:36:38 +0000

    Add files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     counter = 0
     for a in range(x):
         try:
-            print(my_list[a], end='')
+            print(f"{my_list[a]}", end='')
             counter++
         except IndexError:
             pass
